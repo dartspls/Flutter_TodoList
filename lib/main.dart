236f379cb941b55bf4512b5ca622e2a412d2b43e
Shepherd, Daniel Richard Thomas <darts100@gmail.com>
2023-12-18 09:34:34 +1300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const TodoHome(title: 'A todo list in the making'),
+    );
+  }
+}
+
+class TodoHome extends StatelessWidget {
+  const TodoHome({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
+
