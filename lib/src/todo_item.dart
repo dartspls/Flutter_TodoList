5ad264633f69a73370d2f90689469339e0b0f7f2
Shepherd, Daniel Richard Thomas <darts100@gmail.com>
2023-12-18 10:03:06 +1300

    Added created, modified properties to TodoItem

--- lib/src/todo_item.dart
+++ lib/src/todo_item.dart
@@ -1,12 +1,18 @@
 class TodoItem {
-  TodoItem({required String title, String? description}) {
+  TodoItem({required String title, String? description})
+      : created = DateTime.now(),
+        _modiied = DateTime.now() {
     _title = title;
     _description = description;
   }
 
+  final DateTime created;
+
   String _title = '';
   String? _description;
   bool _done = false;
+  bool _pinned = false;
+  DateTime _modiied;
 
   void changeTitle(String newTitle) {
     _title = newTitle;
@@ -16,7 +22,17 @@ class TodoItem {
     _done = val;
   }
 
+  void setPinned(bool val) {
+    _pinned = val;
+  }
+
+  void modify(DateTime dt) {
+    _modiied = dt;
+  }
+
   String get title => _title;
   String get description => _description ?? '';
   bool get isDone => _done;
+  bool get pinned => _pinned;
+  DateTime get modified => _modiied;
 }
